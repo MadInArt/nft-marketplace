dc788ab071e34c20661fa0c92563cadeb379ef82
MadInArt <knyazvladimir26@gmail.com>
2022-10-26 18:56:48 +0200

    add balanceOf and ownerOf methods

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -7,6 +7,17 @@ contract ERC721 {
     mapping(uint256 => address) private _tokenOwner;
     mapping(address => uint256) private _ownedTokensCount;
 
+    function balanceOf(address _owner) public view returns(uint256) {
+        require(_owner != address(0), 'owner query for non-exist token');
+        return _ownedTokensCount[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) public view returns(address){
+        address owner = _tokenOwner[_tokenId];
+        require(owner != address(0), 'onwer query for non-exist token');
+        return owner;
+    }
+
     function _isExists(uint256 tokenId) internal view returns(bool) {
         address owner = _tokenOwner[tokenId];
         return owner != address(0);
@@ -14,6 +25,7 @@ contract ERC721 {
 
     function _mint(address to, uint256 tokenId) internal {
         require(to != address(0), 'ERC721 must be minted to non zero address');
+        require(!_isExists(tokenId), 'Token already been minted');
         _tokenOwner[tokenId] = to;
         _ownedTokensCount[to] += 1;
 
