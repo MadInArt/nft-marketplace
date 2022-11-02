49a58db21c0a8fc7d6a86a01e5c146942cc394f9
MadInArt <knyazvladimir26@gmail.com>
2022-11-02 18:34:11 +0100

    create getTokenByIndex and getTokenByAddressAndIndex functions

--- contracts/ERC721Enumerable.sol
+++ contracts/ERC721Enumerable.sol
@@ -10,16 +10,33 @@ contract ERC721Enumerable is ERC721 {
     mapping(address => uint256[]) private _ownedTokens;
     mapping(uint256 => uint256) private _ownedTokensIndex;
 
-    function totalSupply() public view returns(uint256) {
+    function totalSupply( ) public view returns(uint256) {
         return _totalSupply.length;
     }
 
     function _mint (address to, uint256 tokenId) internal override (ERC721) {
         super._mint(to, tokenId);
-        _addTokensToTotalSupply(tokenId);
+        _addTokensToAllTokenEnumeration(tokenId);
+        _addTokensToOwnerEnumeration(to, tokenId);
     }
 
-    function _addTokensToTotalSupply(uint256 tokenId) private {
+    function _addTokensToAllTokenEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _totalSupply.length;
         _totalSupply.push(tokenId);
+    }
+
+    function _addTokensToOwnerEnumeration(address to, uint256 tokenId) private {
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
+        _ownedTokens[to].push(tokenId);
+    }
+
+    function getTokenByIndex(uint256 index) public view returns(uint256) {
+        require(index < totalSupply(), "Token at the given index doesn't exist");
+        return _totalSupply[index];
+    }
+
+    function getTokenByAddressAndIndex(address owner, uint256 index) public view returns(uint256) {
+        require(index < balanceOf(owner), "Token at the given index of the given user doesn't exist");
+        return _ownedTokens[owner][index];
     }
 }
